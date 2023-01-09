79a14eac4d213a1a694f2b7fed902f00c5d4473f
jenbrissman <brissman514@gmail.com>
2023-01-08 21:49:57 -0500

    cleaning up comments in builder files

--- cloudinary-url.js
+++ cloudinary-url.js
@@ -2,6 +2,6 @@ require("dotenv").config();
 const cloudinary = require("cloudinary").v2;
 cloudinary.config({ secure: "true" });
 
-// create a Cloudinary delivery URL using the CLOUDINARY_URL in the .env file.
+// create a Cloudinary delivery URL using the CLOUDINARY_URL in the config/ .env file.
 
 console.log(cloudinary.url("hiker"));
